5d79093dd6193261ef1f1cfd58d63d691639e992
rm86791 <kaiquysilva63@gmail.com>
2023-06-06 08:29:59 -0300

    detalhes

--- src/Balao.java
+++ src/Balao.java
@@ -1,2 +1,17 @@
 public class Balao {
+    //Atributos
+
+      int id;
+      String registro;
+      String cor;
+      String tipo;
+      int capacidade;
+      double altura;
+      String porte;
+      double peso;
+      //Metodos
+
+    public String voar(){
+        return "voando";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // Construir baloes
+        Balao b1 = new Balao();
+        String resultado = b1.voar();
+        System.out.println(resultado);
 
-
-         String Mario;
-         String Maria;
     }
 }
